86c1af4ee38850353ad1ca59e39becbcb8b07839
Joseph Devaney <Joethesheepfarmer@gmail.com>
2022-01-04 16:11:19 +0000

    add Start function

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+function start() {
+    document.getElementsByClassName('field-div').innerHTML = `
+    <div id="sheep-one" class="sheep-div"><img src="assets/images/mock-sheep.png" class="sheep"
+        id="mock-sheep-one-image"></div>
+    <div id="sheep-two" class="sheep-div"><img src="assets/images/mock-sheep.png" class="sheep"
+        id="mock-sheep-two-image"></div>
+    <div id="sheep-three" class="sheep-div"><img src="assets/images/mock-sheep.png" class="sheep"
+        id="mock-sheep-three-image"></div>
+    <div id="sheep-four" class="sheep-div"><img src="assets/images/mock-sheep.png" class="sheep"
+        id="mock-sheep-four-image"></div>
+    <div id="sheep-five" class="sheep-div"><img src="assets/images/mock-sheep.png" class="sheep"
+        id="mock-sheep-five-image"></div>
+    <div id="sheep-six" class="sheep-div"><img src="assets/images/mock-sheep.png" class="sheep"
+        id="mock-sheep-six-image"></div> `;
+}
