bf8712350362173d579c550bc9dbbf9e18022261
Joseph Devaney <Joethesheepfarmer@gmail.com>
2022-01-06 12:35:32 +0000

    layed out the functions for the project in the java script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,79 @@
-function start() {
-    document.getElementsByClassName('field-div').innerHTML = `
-    <div id="sheep-one" class="sheep-div"><img src="assets/images/mock-sheep.png" class="sheep"
-        id="mock-sheep-one-image"></div>
-    <div id="sheep-two" class="sheep-div"><img src="assets/images/mock-sheep.png" class="sheep"
-        id="mock-sheep-two-image"></div>
-    <div id="sheep-three" class="sheep-div"><img src="assets/images/mock-sheep.png" class="sheep"
-        id="mock-sheep-three-image"></div>
-    <div id="sheep-four" class="sheep-div"><img src="assets/images/mock-sheep.png" class="sheep"
-        id="mock-sheep-four-image"></div>
-    <div id="sheep-five" class="sheep-div"><img src="assets/images/mock-sheep.png" class="sheep"
-        id="mock-sheep-five-image"></div>
-    <div id="sheep-six" class="sheep-div"><img src="assets/images/mock-sheep.png" class="sheep"
-        id="mock-sheep-six-image"></div> `;
+
+function start(){
+    // add sheep to field
+    var startTime = currentTime;
+    var SheepCount = 0;
+};
+
+function clickSheepOne (){
+    // remove sheepOne from field-div
+    // add sheepOne to tractor-div
+    SheepCount++;
+    if (SheepCount = 6){
+        Terminate()
+    };
+};
+
+function clickSheepTwo (){
+    // remove sheepTwo from field-div
+    // add sheepTwo to tractor-div
+    SheepCount++;
+    if (SheepCount = 6){
+        Terminate()
+    };
+};
+
+function clickSheepThree (){
+    // remove sheepThree from field-div
+    // add sheepThree to tractor-div
+    SheepCount++;
+    if (SheepCount = 6){
+        Terminate()
+    };
+};
+
+function clickSheepFour (){
+    // remove sheepFour from field-div
+    // add sheepFour to tractor-div
+    SheepCount++;
+    if (SheepCount = 6){
+        Terminate()
+    };
+};
+
+function clickSheepFive (){
+    // remove sheepFive from field-div
+    // add sheepFive to tractor-div
+    SheepCount++;
+    if (SheepCount = 6){
+        Terminate()
+    };
+};
+
+function clickSheepSix (){
+    // remove sheepSix from field-div
+    // add sheepSix to tractor-div
+    SheepCount++;
+    if (SheepCount = 6){
+        Terminate()
+    };
+};
+
+function Terminate (){
+    document.getElementById("field-div").innerHTML =`
+        <h2>well done!</h2>
+        <p>your time was:<span id="score-span"></span></p>
+        <button onclick="restart()">Restart</button>`;
+   score();
 }
+
+function score(){
+    var endTime = currentTime
+    var time = endTime - startTime
+    document.getElementById("score-span").innerHTML = `${time} seconds!`
+
+};
+
+function restart(){
+    //reload page
+};
