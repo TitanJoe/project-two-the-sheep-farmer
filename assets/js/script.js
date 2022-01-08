d2d0a00a05bd8d4119f77558bc0d103464f3f76f
Joseph Devaney <Joethesheepfarmer@gmail.com>
2022-01-08 14:11:52 +0000

    styled webpage responsiveness for screen sizes 1440px, 1040px and 768px

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,78 +1,78 @@
 let sheepCount = 0;
-let startTime;
-let endTime;
+let startTime = null;
+let endTime = null;
 
-function start(){
+function start() {
     document.getElementById("start-menu").style.visibility = 'hidden';
     document.getElementById("sheep-field").style.visibility = 'visible';
     startTime = Date.now();
 };
 
-function sheepOne(){
+function sheepOne() {
     document.getElementsByClassName("field-sheep-imgs")[0].style.visibility = 'hidden';
     document.getElementsByClassName("trailer-sheep-imgs")[5].style.visibility = 'visible';
     sheepCount++;
-    if(sheepCount === 6){
+    if (sheepCount === 6) {
         terminate()
     }
 }
 
-function sheepTwo(){
+function sheepTwo() {
     document.getElementsByClassName("field-sheep-imgs")[1].style.visibility = 'hidden';
     document.getElementsByClassName("trailer-sheep-imgs")[4].style.visibility = 'visible';
     sheepCount++;
-    if(sheepCount === 6){
+    if (sheepCount === 6) {
         terminate()
     }
 }
 
-function sheepThree(){
+function sheepThree() {
     document.getElementsByClassName("field-sheep-imgs")[2].style.visibility = 'hidden';
     document.getElementsByClassName("trailer-sheep-imgs")[3].style.visibility = 'visible';
     sheepCount++;
-    if(sheepCount === 6){
+    if (sheepCount === 6) {
         terminate()
     }
 }
 
-function sheepFour(){
+function sheepFour() {
     document.getElementsByClassName("field-sheep-imgs")[3].style.visibility = 'hidden';
     document.getElementsByClassName("trailer-sheep-imgs")[2].style.visibility = 'visible';
     sheepCount++;
-    if(sheepCount === 6){
+    if (sheepCount === 6) {
         terminate()
     }
 }
 
-function sheepFive(){
+function sheepFive() {
     document.getElementsByClassName("field-sheep-imgs")[4].style.visibility = 'hidden';
     document.getElementsByClassName("trailer-sheep-imgs")[1].style.visibility = 'visible';
     sheepCount++;
-    if(sheepCount === 6){
+    if (sheepCount === 6) {
         terminate()
     }
 }
 
-function sheepSix(){
+function sheepSix() {
     document.getElementsByClassName("field-sheep-imgs")[5].style.visibility = 'hidden';
     document.getElementsByClassName("trailer-sheep-imgs")[0].style.visibility = 'visible';
     sheepCount++;
-    if(sheepCount === 6){
+    if (sheepCount === 6) {
         terminate()
     }
 }
 
-function terminate(){
+function terminate() {
     document.getElementById("end-menu").style.visibility = 'visible';
     score()
 }
 
-function score(){
+function score() {
     endTime = Date.now();
     let yourTime = Math.floor(((endTime - startTime) / 1000) * 10) / 10;
     document.getElementById("score-span").innerHTML = yourTime;
 }
 
-function restart (){
+function restart() {
     location.reload()
 }
